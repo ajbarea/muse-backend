4fb2093610723ca5e9e2285be331b25f64ff1d4a
AJ Barea <ajbareaa@gmail.com>
2024-06-19 19:27:07 -0700

    initial commit

---
+++ src/test/java/com/musemaker/muse_backend/MuseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musemaker.muse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
